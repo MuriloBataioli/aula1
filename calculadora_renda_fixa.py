61ea32dd6b0c27656028ae5fa3adcd44756a426c
MuriloBataioli <murilobataioli83@gmail.com>
2024-11-04 14:40:12 -0400

    Update calculadora_renda_fixa.py

--- calculadora_renda_fixa.py
+++ calculadora_renda_fixa.py
@@ -4,6 +4,6 @@ capital = float(input('quanto voçê vai aplicar? '))
 taxa = float(input('qual é a taxa de juros anual? '))
 tempo = float(input('Qual é o tempo de aplicacão? '))
 montante = capital * (1 + (taxa/100))**tempo
-juros = mmontante - capital
+juros = montante - capital
 montante = capital + juros
 print(f'O valor total do seu investimento à {tempo} anos atras te rendeu um total de R${math.ceil(montante)}')
