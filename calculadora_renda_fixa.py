54e4261f2b87a386d242a2df59c636ed30f79a25
MuriloBataioli <murilobataioli83@gmail.com>
2024-11-04 14:39:49 -0400

    Update calculadora_renda_fixa.py

--- calculadora_renda_fixa.py
+++ calculadora_renda_fixa.py
@@ -1,9 +1,9 @@
 import math
 print('Olá eu sou uma calculadora de renda fixa vamos multiplicar a sua grana?')
-c = float(input('quanto voçê vai aplicar? '))
-i = float(input('qual é a taxa de juros anual? '))
-n = float(input('Qual é o tempo de aplicacão? '))
-m = c * (1 + (i/100))**n
-j = m - c
-m = c + j
-print(f'O valor total do seu investimento à {n} anos atras te rendeu um total de R${math.ceil(m)}')
+capital = float(input('quanto voçê vai aplicar? '))
+taxa = float(input('qual é a taxa de juros anual? '))
+tempo = float(input('Qual é o tempo de aplicacão? '))
+montante = capital * (1 + (taxa/100))**tempo
+juros = mmontante - capital
+montante = capital + juros
+print(f'O valor total do seu investimento à {tempo} anos atras te rendeu um total de R${math.ceil(montante)}')
