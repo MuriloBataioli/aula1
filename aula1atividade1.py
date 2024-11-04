1b4216ace496c928097828b93094dc6090b5cfa6
MuriloBataioli <murilobataioli83@gmail.com>
2024-11-04 18:16:53 +0000

    mensagem :(

--- aula1atividade1.py
+++
@@ -1,5 +0,0 @@
-print('Olá usuario vamos calcular seu IMC')
-b = float(input('Seu peso em kg: '))
-a = float(input('Sua altura em metros: '))
-c = b / a**2
-print(f'Seu IMC deu {c:.2f}')
--- aula1atividade3.py
+++
@@ -1,9 +0,0 @@
-import math
-print('vamos calcular equação de segundo grau que legal :(')
-a = float(input('Escreva o primeiro numero '))
-b = float(input('Escreva o segundo numero '))
-c = float(input('Escreva o terceiro numero '))
-d = float(input('escreva o quarto numero '))
-x1 = ((-b) + (b**2-4*a*(c - d))**0.5)/(2*a)
-x2 = ((-b) - (b**2-4*a*(c - d))**0.5)/(2*a)
-print(f'x1={x1} e x2={x2}')
